a8347168d053a566d9232c9928964b6714b57601
Vedant Hule <92876956+vedant0000@users.noreply.github.com>
2025-08-12 15:00:51 +0530

    Updated RoundRobin

--- RoundRobin.java
+++ RoundRobin.java
@@ -1,17 +1,5 @@
 import java.util.*;
 
-class Process {
-    int pid, arrivalTime, burstTime, remainingTime, completionTime, turnAroundTime, waitingTime;
-    boolean isCompleted = false;
-
-    Process(int pid, int at, int bt) {
-        this.pid = pid;
-        this.arrivalTime = at;
-        this.burstTime = bt;
-        this.remainingTime = bt;
-    }
-}
-
 public class RoundRobin {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -20,31 +8,40 @@ public class RoundRobin {
         System.out.print("Enter number of processes: ");
         int n = sc.nextInt();
 
-        Process[] processes = new Process[n];
+        int[] pid = new int[n];
+        int[] at = new int[n];
+        int[] bt = new int[n];
+        int[] rt = new int[n]; // remaining time
+        int[] ct = new int[n];
+        int[] tat = new int[n];
+        int[] wt = new int[n];
+        boolean[] completed = new boolean[n];
+        boolean[] inQueue = new boolean[n];
 
+        // Input process details
         for (int i = 0; i < n; i++) {
-            System.out.println("\nEnter details for Process " + (i + 1) + ":");
+            pid[i] = i + 1;
+            System.out.println("\nEnter details for Process " + pid[i] + ":");
             System.out.print("Arrival Time: ");
-            int at = sc.nextInt();
+            at[i] = sc.nextInt();
             System.out.print("Burst Time: ");
-            int bt = sc.nextInt();
-            processes[i] = new Process(i + 1, at, bt);
+            bt[i] = sc.nextInt();
+            rt[i] = bt[i];
         }
 
         System.out.print("\nEnter Time Quantum: ");
         int tq = sc.nextInt();
 
-        Queue<Process> queue = new LinkedList<>();
-        int currentTime = 0, completed = 0;
+        Queue<Integer> queue = new LinkedList<>();
+        int currentTime = 0, completedCount = 0;
         double totalTAT = 0, totalWT = 0;
-        boolean[] isInQueue = new boolean[n];
 
-        while (completed < n) {
-            // Add newly arrived processes to queue
+        while (completedCount < n) {
+            // Add newly arrived processes to the queue
             for (int i = 0; i < n; i++) {
-                if (processes[i].arrivalTime <= currentTime && !isInQueue[i] && processes[i].remainingTime > 0) {
-                    queue.add(processes[i]);
-                    isInQueue[i] = true;
+                if (at[i] <= currentTime && !inQueue[i] && rt[i] > 0) {
+                    queue.add(i);
+                    inQueue[i] = true;
                 }
             }
 
@@ -53,39 +50,39 @@ public class RoundRobin {
                 continue;
             }
 
-            Process current = queue.poll();
-
-            int execTime = Math.min(tq, current.remainingTime);
-            current.remainingTime -= execTime;
+            int idx = queue.poll(); // index of current process
+            int execTime = Math.min(tq, rt[idx]);
+            rt[idx] -= execTime;
             currentTime += execTime;
 
             // Add new arrivals during this time quantum
             for (int i = 0; i < n; i++) {
-                if (processes[i].arrivalTime <= currentTime && !isInQueue[i] && processes[i].remainingTime > 0) {
-                    queue.add(processes[i]);
-                    isInQueue[i] = true;
+                if (at[i] <= currentTime && !inQueue[i] && rt[i] > 0) {
+                    queue.add(i);
+                    inQueue[i] = true;
                 }
             }
 
-            if (current.remainingTime == 0 && !current.isCompleted) {
-                current.completionTime = currentTime;
-                current.turnAroundTime = current.completionTime - current.arrivalTime;
-                current.waitingTime = current.turnAroundTime - current.burstTime;
+            if (rt[idx] == 0 && !completed[idx]) {
+                ct[idx] = currentTime;
+                tat[idx] = ct[idx] - at[idx];
+                wt[idx] = tat[idx] - bt[idx];
 
-                totalTAT += current.turnAroundTime;
-                totalWT += current.waitingTime;
+                totalTAT += tat[idx];
+                totalWT += wt[idx];
 
-                current.isCompleted = true;
-                completed++;
+                completed[idx] = true;
+                completedCount++;
             } else {
-                queue.add(current); // Add back to queue if not finished
+                queue.add(idx); // put back into queue if not finished
             }
         }
 
+        // Output
         System.out.println("\nPID\tAT\tBT\tCT\tTAT\tWT");
-        for (Process p : processes) {
-            System.out.println("P" + p.pid + "\t" + p.arrivalTime + "\t" + p.burstTime + "\t" +
-                    p.completionTime + "\t" + p.turnAroundTime + "\t" + p.waitingTime);
+        for (int i = 0; i < n; i++) {
+            System.out.println("P" + pid[i] + "\t" + at[i] + "\t" + bt[i] + "\t" +
+                    ct[i] + "\t" + tat[i] + "\t" + wt[i]);
         }
 
         System.out.printf("\nAverage Turnaround Time: %.2f", totalTAT / n);
